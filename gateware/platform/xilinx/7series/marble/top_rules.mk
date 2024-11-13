8dcecde6f2032445dfd040dcdc440358f1b48337
Alessio Amodio <aamodio@lbl.gov>
2024-11-13 11:38:10 -0800

    gateware/platform/**/top_rules.mk: added xci files in cell_comm_IP_CORES

--- gateware/platform/xilinx/7series/marble/top_rules.mk
+++ gateware/platform/xilinx/7series/marble/top_rules.mk
@@ -3,7 +3,18 @@ cell_comm_marble_platform_DIR = $(CELL_COMM_PLATFORM_DIR)xilinx/7series/marble/
 cell_comm_IP_CORES = \
 	fofbReadLinksMux \
 	forwardCellLinkMux \
-	readBPMlinksMux
+	readBPMlinksMux \
+	axisDataFifo32 \
+	axiStreamDwUpcon \
+	axiStreamSubConvUpcon \
+	axiStreamClkConvUpcon \
+	axiStreamClkConvDowncon \
+	axiStreamDwDowncon \
+	axiStreamSubConvInDowncon \
+	axiStreamSubConvOutDowncon \
+	evrmgt \
+	aurora64b66b
+
 cell_comm_IP_CORES_DIRS = $(addprefix $(cell_comm_marble_platform_DIR), $(cell_comm_IP_CORES))
 
 # For top-level makefile
