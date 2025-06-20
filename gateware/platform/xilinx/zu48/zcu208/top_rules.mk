334ff63c38439ed043fc3fd14c87ff77caafdca3
Lucas Russo <lerwys@gmail.com>
2025-06-20 14:03:16 -0700

    platform: add aurora64b66b IPs for zu47/zu48 parts

---
+++ gateware/platform/xilinx/zu48/zcu208/top_rules.mk
@@ -0,0 +1,20 @@
+cell_comm_marble_platform_DIR = $(CELL_COMM_PLATFORM_DIR)xilinx/7series/marble/
+
+cell_comm_IP_CORES = \
+	axisDataFifo32 \
+	axiStreamDwUpcon \
+	axiStreamSubConvUpcon \
+	axiStreamClkConvUpcon \
+	axiStreamClkConvDowncon \
+	axiStreamDwDowncon \
+	axiStreamSubConvInDowncon \
+	axiStreamSubConvOutDowncon \
+	aurora64b66b
+
+cell_comm_IP_CORES_DIRS = $(addprefix $(cell_comm_marble_platform_DIR), $(cell_comm_IP_CORES))
+
+# For top-level makefile
+IP_CORES_XCIS += $(addsuffix .xci, $(cell_comm_IP_CORES))
+IP_CORES_DIRS += $(cell_comm_IP_CORES_DIRS)
+
+vpath %.xci $(IP_CORES_DIRS)
