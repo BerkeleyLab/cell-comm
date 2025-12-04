fca3618593b23be3de95afebc72bfb314a508b6e
Lucas Russo <lerwys@gmail.com>
2025-12-03 16:59:21 -0800

    platform: fix safer clean rules for IP cores

--- gateware/platform/xilinx/zu48/zcu208/bottom_rules.mk
+++ gateware/platform/xilinx/zu48/zcu208/bottom_rules.mk
@@ -1,3 +1,6 @@
 # clean generate IP cores files, but the source ones (.xci or .bd)
 clean::
-	$(foreach ipcore, $(cell_comm_IP_CORES), find $(cell_comm_zcu208_platform_DIR)$(ipcore) -mindepth 1 -not \( -name \*$(ipcore).xci -o -name \*$(ipcore).bd \) -delete ;)
+	$(foreach ipcore, \
+		$(cell_comm_IP_CORES), \
+		test -f $(cell_comm_zcu208_platform_DIR)$(ipcore)/$(ipcore).xci && \
+		find $(cell_comm_zcu208_platform_DIR)$(ipcore) -mindepth 1 -not \( -name \*$(ipcore).xci -o -name \*$(ipcore).bd -o -name \*$(ipcore).coe \) -delete ;)
