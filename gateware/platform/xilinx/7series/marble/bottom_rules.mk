5283837e6562c644101985e0528293a5e6d02551
Lucas Russo <lerwys@gmail.com>
2026-06-22 14:26:03 -0700

    platform: fix wrong clean rule for 7-series Marble

--- gateware/platform/xilinx/7series/marble/bottom_rules.mk
+++ gateware/platform/xilinx/7series/marble/bottom_rules.mk
@@ -2,5 +2,5 @@
 clean::
 	$(foreach ipcore, \
 		$(cell_comm_IP_CORES), \
-		test -f $(cell_comm_marble_platform_DIR)$(ipcore)/$(ipcore).xci && \
-		find $(cell_comm_marble_platform_DIR)$(ipcore) -mindepth 1 -not \( -name \*$(ipcore).xci -o -name \*$(ipcore).bd -o -name \*$(ipcore).coe \) -delete ;)
+		test -f $(cell_comm_marble_platform_DIR)$(ipcore)/$(ipcore).tcl && \
+		find $(cell_comm_marble_platform_DIR)$(ipcore) -mindepth 1 -not \( -name \*$(ipcore).tcl -o -name \*$(ipcore).bd -o -name \*$(ipcore).coe \) -delete ;)
